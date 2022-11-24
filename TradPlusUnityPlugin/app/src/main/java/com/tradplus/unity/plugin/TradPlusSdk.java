4a52ac5e281dac4a8db5a55198b8453c85621247
fanny <service@tradplus.com>
2022-11-24 16:35:59 +0800

    v1.0.4

--- TradPlusUnityPlugin/app/src/main/java/com/tradplus/unity/plugin/TradPlusSdk.java
+++ TradPlusUnityPlugin/app/src/main/java/com/tradplus/unity/plugin/TradPlusSdk.java
@@ -2,11 +2,14 @@ package com.tradplus.unity.plugin;
 
 import android.util.Log;
 
+import com.tradplus.ads.base.bean.TPAdInfo;
 import com.tradplus.ads.base.common.TPDiskManager;
 import com.tradplus.ads.base.common.TPPrivacyManager;
 import com.tradplus.ads.common.ClientMetadata;
+import com.tradplus.ads.common.serialization.JSON;
 import com.tradplus.ads.common.util.Json;
 import com.tradplus.ads.core.AdCacheManager;
+import com.tradplus.ads.core.GlobalImpressionManager;
 import com.tradplus.ads.mobileads.gdpr.Const;
 import com.tradplus.ads.mobileads.util.SegmentUtils;
 import com.tradplus.ads.mobileads.util.TestDeviceUtil;
@@ -26,23 +29,22 @@ public class TradPlusSdk extends BaseUnityPlugin {
         return sInstance;
     }
 
-    public static void initSdk(String appId,TPInitListener tradPlusInitListener){
-        Log.i("TradPlus","init Start");
+    public static void initSdk(String appId, TPInitListener tradPlusInitListener) {
+        Log.i("TradPlus", "init Start");
         com.tradplus.ads.open.TradPlusSdk.setTradPlusInitListener(new com.tradplus.ads.open.TradPlusSdk.TradPlusInitListener() {
             @Override
             public void onInitSuccess() {
-                Log.i("TradPlus","init success tradPlusInitListener = "+tradPlusInitListener);
-                if(tradPlusInitListener != null){
+                Log.i("TradPlus", "init success tradPlusInitListener = " + tradPlusInitListener);
+                if (tradPlusInitListener != null) {
                     tradPlusInitListener.onResult("true");
                 }
             }
         });
 
-        com.tradplus.ads.open.TradPlusSdk.initSdk(getActivity(),appId);
+        com.tradplus.ads.open.TradPlusSdk.initSdk(getActivity(), appId);
     }
 
-    public static void initCustomMap(String map)
-    {
+    public static void initCustomMap(String map) {
         SegmentUtils.initCustomMap(Json.jsonStringToMap(map));
     }
 
@@ -52,14 +54,14 @@ public class TradPlusSdk extends BaseUnityPlugin {
             @Override
             public void onSuccess(boolean isEu, boolean isCn, boolean isCa) {
 
-                if(listener != null){
-                    listener.onSuccess(isEu,isCn,isCa);
+                if (listener != null) {
+                    listener.onSuccess(isEu, isCn, isCa);
                 }
             }
 
             @Override
             public void onFailed() {
-                if(listener != null){
+                if (listener != null) {
                     listener.onFailed();
                 }
             }
@@ -72,7 +74,7 @@ public class TradPlusSdk extends BaseUnityPlugin {
         com.tradplus.ads.open.TradPlusSdk.showUploadDataNotifyDialog(getActivity(), new com.tradplus.ads.open.TradPlusSdk.TPGDPRAuthListener() {
             @Override
             public void onAuthResult(int i) {
-                if(tpgdprAuthListener != null){
+                if (tpgdprAuthListener != null) {
                     tpgdprAuthListener.onAuthResult(i);
                 }
             }
@@ -83,11 +85,11 @@ public class TradPlusSdk extends BaseUnityPlugin {
         return ClientMetadata.getInstance(getActivity()).getSdkVersion();
     }
 
-    public static boolean isEUTraffic(){
+    public static boolean isEUTraffic() {
         return com.tradplus.ads.open.TradPlusSdk.isEUTraffic(getActivity());
     }
 
-    public static boolean isCalifornia(){
+    public static boolean isCalifornia() {
         return com.tradplus.ads.open.TradPlusSdk.isCalifornia(getActivity());
     }
 
@@ -97,6 +99,14 @@ public class TradPlusSdk extends BaseUnityPlugin {
 
     public static int getGDPRDataCollection() {
         return com.tradplus.ads.open.TradPlusSdk.getGDPRDataCollection(getActivity());
+    }
+
+    public static void setLGPDConsent(boolean consent) {
+        com.tradplus.ads.open.TradPlusSdk.setLGPDConsent(getActivity(), consent ? 0 : 1);
+    }
+
+    public static int getLGPDConsent() {
+        return com.tradplus.ads.open.TradPlusSdk.getLGPDConsent(getActivity());
     }
 
     public static void setCCPADoNotSell(boolean canDataCollection) {
@@ -133,9 +143,9 @@ public class TradPlusSdk extends BaseUnityPlugin {
         return com.tradplus.ads.open.TradPlusSdk.isOpenPersonalizedAd();
     }
 
-    public static void clearCache(String unitId){
+    public static void clearCache(String unitId) {
         int readyAdNum = AdCacheManager.getInstance().getReadyAdNum(unitId);
-        AdCacheManager.getInstance().removeEndCache(unitId,readyAdNum);
+        AdCacheManager.getInstance().removeEndCache(unitId, readyAdNum);
     }
 
     public static void setPrivacyUserAgree(boolean open) {
@@ -143,31 +153,43 @@ public class TradPlusSdk extends BaseUnityPlugin {
     }
 
     public static boolean isPrivacyUserAgree() {
-       return com.tradplus.ads.open.TradPlusSdk.isPrivacyUserAgree();
+        return com.tradplus.ads.open.TradPlusSdk.isPrivacyUserAgree();
     }
 
-    public static void setMaxDatabaseSize(long size){
+    public static void setMaxDatabaseSize(long size) {
         TPDiskManager.getInstance().setMaxDatabaseSize(size);
     }
 
-    public static void setTestDevice(boolean isTestDevice,String testModeId){
-        TestDeviceUtil.getInstance().setNeedTestDevice(isTestDevice,testModeId);
+    public static void setTestDevice(boolean isTestDevice, String testModeId) {
+        TestDeviceUtil.getInstance().setNeedTestDevice(isTestDevice, testModeId);
     }
 
-    public static void setAutoExpiration(boolean isAuto){
+    public static void setAutoExpiration(boolean isAuto) {
         com.tradplus.ads.open.TradPlusSdk.setAutoExpiration(isAuto);
     }
 
-    public static void checkAutoExpiration(){
+    public static void checkAutoExpiration() {
         com.tradplus.ads.open.TradPlusSdk.checkAutoExpiration();
     }
 
-    public static void setCnServer(boolean isCn){
+    public static void setCnServer(boolean isCn) {
         com.tradplus.ads.open.TradPlusSdk.setCnServer(isCn);
     }
 
-    public static void setOpenDelayLoadAds(boolean isOpen){
+    public static void setOpenDelayLoadAds(boolean isOpen) {
         com.tradplus.ads.open.TradPlusSdk.setOpenDelayLoadAds(isOpen);
     }
+
+    public static void setGlobalImpressionListener(TPGlobalImpressionListener tPGlobalImpressionListener) {
+        com.tradplus.ads.open.TradPlusSdk.setGlobalImpressionListener(new GlobalImpressionManager.GlobalImpressionListener() {
+            @Override
+            public void onImpressionSuccess(TPAdInfo tpAdInfo) {
+                if (tPGlobalImpressionListener != null) {
+                    tPGlobalImpressionListener.onImpressionSuccess(JSON.toJSONString(tpAdInfo));
+                }
+            }
+        });
+    }
+
 
 }
